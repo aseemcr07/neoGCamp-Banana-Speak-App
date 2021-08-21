db59c1b289a31ecd3c5716ac38d19ae9a8903f01
aseemcr07 <60442085+aseemcr07@users.noreply.github.com>
2021-08-21 19:21:00 +0530

    Input processing output done

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var textInput = document.querySelector("#text-input");
+var outputDiv = document.querySelector("#output");
 
 function clickHandler(){
-    console.log("Clicked!");
-    console.log("Input: ", textInput.value);
+    outputDiv.innerText = "ajajajajajaj " + textInput.value;
 }
 
 btnTranslate.addEventListener("click", clickHandler)
